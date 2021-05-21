a74b292193450dc296847529e13cd798fc582908
Nick McDaniel <nickamcd@gmail.com>
2021-05-21 14:51:34 -0700

    Added initial player factory and game/gameboard modules

---
+++ script.js
@@ -0,0 +1,24 @@
+// GameBoard module
+const GameBoard = (() => {
+  let gameboard = [];
+
+
+})();
+
+// Game module
+const Game = (() => {
+
+})();
+
+// Player factory
+const Player = (marker) => {
+  const getMarker = () => marker;
+
+  return {getMarker}
+};
+
+const playerOne = Player('x');
+const playerTwo = Player('o');
+
+console.log(playerOne.getMarker())
+console.log(playerTwo.getMarker())
